d1f65dde381cc22efe856d487c1702c65804642c
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-20 09:40:47 +0300

    Finishes lab

---
+++ src/components/DirectorCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const DirectorCard = ({director}) => {
+
+  return (
+    <article>
+        <h2>{director.name}</h2>
+        <ul>
+            <li>
+                {/* Should render a list of movies from director.movies */}
+            {director.movies}
+            </li>
+        </ul>
+    </article>
+  )
+}
+
+export default DirectorCard
